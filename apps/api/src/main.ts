f4cea176fc7f06bac5123167aa56002f4fc7064b
erik <eabnelson@gmail.com>
2023-07-07 15:36:10 -0400

    trustin

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
 
 	const redisClient = app.get<Redis.Redis>('REDIS_CLIENT');
 
-	app.enable('trust proxy');
+	app.enable('trust proxy', true);
 
 	const sessionStore = new RedisStore({
 		client: redisClient
